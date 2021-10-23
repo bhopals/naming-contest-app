27ea3216986bef37a58b163be7f588c7b730ff8f
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-23 17:30:19 -0400

    CRUD Operations

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 import config from './config';
 import apiRouter from './api';
-
+import serverRender from './serverRender';
 import express from 'express';
-const server = express();
+import bodyParser from 'body-parser';
 
-server.get('/', (req, res) => {
-  res.send('Hello Express');
+const server = express();
+server.use(bodyParser.json());
+
+server.set('view engine', 'ejs');
+
+server.get(['/', '/contest/:contestId'], (req, res) => {
+  serverRender(req.params.contestId)
+    .then(({ initialMarkup, initialData }) => {
+      res.render('index', {
+        initialMarkup,
+        initialData
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(404).send('Bad Request');
+    });
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () => {
+server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
 });
